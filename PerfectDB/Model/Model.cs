ad3cfd36eccdf3462dbb97db78c1f7413fb5b07a
Danil Zhigamovskyi <danil.zhigamovskyi@gmail.com>
2018-04-10 13:44:35 +0300

    first commit

---
+++ PerfectDB/Model/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient; // в этом пространстве имен находится поставщик данных для MS SQLServer
+
+namespace PerfectDB.Model
+{
+    class DBData
+    {
+        SqlConnection connection;
+        public DBData(string dataSource)
+        {
+            SqlConnectionStringBuilder conStrBuilder = new SqlConnectionStringBuilder();
+            conStrBuilder.DataSource = dataSource;
+            conStrBuilder.InitialCatalog = "Shop";
+            conStrBuilder.IntegratedSecurity = true;
+            connection = new SqlConnection(conStrBuilder.ConnectionString);
+        }
+        public bool ExecuteSql(string command)
+        {
+            SqlCommand cmnd = new SqlCommand(command);
+            cmnd.
+        }
+    }
+}
